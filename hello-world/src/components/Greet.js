d5ccabc94abfda5042a3021b0a22f98e59a9a275
dhanaraj ccs <dhanarajccs@gmail.com>
2022-03-17 16:36:45 +0530

    Functional Component - ES6 Fat Arrow

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -27,7 +27,7 @@ export default Greet
 
 
 // Functional Component - Normal function
-
+/*
 import React from 'react'                   // rfce
 
 function Greet() {
@@ -52,3 +52,120 @@ export default function Greet() {
     )
 }
 */
+
+
+
+// Functional Component - ES6 Arrow Function (Fat Arrow Function) - syntax
+/*
+// Creates a React Arrow Function Component with ES7 module system   
+import React from 'react'                   // rafc
+
+export const Greet = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+// Creates a React Arrow Function Component with ES7 module system 
+import React from 'react'                   // rafce
+
+const Greet = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Greet
+
+
+// Creates a React 17 Arrow Function Component with ES7 module system
+export const Greet = () => {               // _rafc
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+// Creates a React 17 Arrow Function Component with ES7 module system
+const Greet = () => {                      // _rafce
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Greet
+*/
+
+
+// Functional Component - ES6 Arrow Function (fat Arrow Function)
+
+// Default Export (export default)
+
+import React from 'react'               // rafce
+
+const Greet = () => {
+    return (
+        <div>
+            <h1>Dhanaraj Narasimmareddy Prasanth</h1>
+        </div>
+    )
+}
+
+export default Greet
+
+// or
+/* 
+import React from 'react'
+
+const Greet = () => {
+    return <h1>Dhanaraj Narasimmareddy Prasanth</h1>    
+}
+
+export default Greet
+
+// or 
+
+import React from 'react';
+
+const Greet = () => <h1>Dhanaraj Narasimmareddy Prasanth</h1>
+
+export default Greet
+*/
+
+
+
+// Named Export (export)
+/*
+import React from 'react';          // rafc
+
+export const Greet = () => {
+  return (
+    <div>
+      <h1>Dhanaraj Narasimmareddy Prasanth</h1>
+    </div>
+  );
+}
+
+// or
+
+import React from 'react';
+
+export const Greet = () => {
+  return <h1>Dhanaraj Narasimmareddy Prasanth</h1>
+}
+
+// or 
+
+import React from 'react';
+
+export const Greet = () => <h1>Dhanaraj Narasimmareddy Prasanth</h1> // import as { Greet } in App.js
+*/
+
+
