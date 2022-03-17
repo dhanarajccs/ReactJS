2ca75165b31fac0e4c69f13be1ad1bc682d27bde
dhanaraj ccs <dhanarajccs@gmail.com>
2022-03-17 16:31:46 +0530

    Functional Component - Normal

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,54 @@
+// Functional Component - syntax
+/*
+import React from 'react'
+
+export default function Greet() {           // rfc
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+// or
+
+import React from 'react'                   // rfce
+
+function Greet() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Greet
+*/
+
+
+// Functional Component - Normal function
+
+import React from 'react'                   // rfce
+
+function Greet() {
+    return (
+        <div>
+            <h1>Dhanaraj Narasimmareddy Prasanth</h1>
+        </div>
+    )
+}
+
+export default Greet
+/*
+// or
+
+import React from 'react'                    // rfc
+
+export default function Greet() {
+    return (
+        <div>
+          <h1>Dhanaraj Narasimmareddy Prasanth</h1>
+        </div>
+    )
+}
+*/
